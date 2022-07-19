280f457c98d472162ea4a962fcfdb223ca6c261f
Mohamed Abdulhakim <69153321+MohamedCK@users.noreply.github.com>
2022-07-19 10:00:04 +0200

    Fix linter errors in the js file

--- Index.js
+++ Index.js
@@ -1,13 +1,19 @@
 const menuIcon = document.querySelector('.mob-menu');
 const sideNav = document.querySelector('.mobileNav');
 const menuLinks = document.querySelectorAll('.menuMobile');
+const main = document.querySelector('main');
+// let body = document.querySelector('body');
 
 const open = () => {
   sideNav.classList.add('mobileNavActive');
+  main.style.opacity = '0.2';
+  main.style.backdropfilter = 'blur(8px)';
+  // body.style.color = '#6070FF';
 };
 
 const close = () => {
   sideNav.classList.remove('mobileNavActive');
+  main.style.opacity = '1';
 };
 
 menuIcon.addEventListener('click', open);
